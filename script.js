9be1027117ab5ee9bb6ff58846e3625d8f27baa9
rt2171998 <70574052+rt2171998@users.noreply.github.com>
2023-06-18 15:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-window.promises = [];
 
+function getRandomTime(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+function createPromise() {
+      const randomTime = getRandomTime(1000, 5000);
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(`Resolved after ${randomTime} milliseconds`);
+        }, randomTime);
+      });
+    }
+const promises = Array.from({ length: 5 }, createPromise);
+window.promises = [promises];
+Promise.any(promises)
+      .then(result => {
+        document.getElementById("output").textContent = result;
+      })
+      .catch(error => {
+        console.log("All promises rejected:", error);
+      });
 // Do not change the code above this
 // add your promises to the array `promises`
